74778ae2e94fb905cc322cf29a5ef12c32ae95ae
Juan Arango <115046001+juanarangodata@users.noreply.github.com>
2022-10-04 18:21:19 -0400

    Update stream_app_.py

--- stream_app_.py
+++ stream_app_.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title( "MY son plays toomuch") 
 streamlit.text('que se dice socio ') 
+streamlit.header('Breakfast Menu')
+streamlit.text(' 🥣 🥗 🐔 🥑🍞 Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
